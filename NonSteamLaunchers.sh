ba763e6cdf318cf1b17af99b213bf76222df17f3
moraroy <88516395+moraroy@users.noreply.github.com>
2023-05-05 19:29:54 -0700

    Update NonSteamLaunchers.sh
    oops

--- NonSteamLaunchers.sh
+++ NonSteamLaunchers.sh
@@ -1825,7 +1825,7 @@ with open('$shortcuts_vdf_path', 'wb') as f:
 
 
 # Delete NonSteamLaunchersInstallation subfolder in Downloads folder
-#rm -rf ~/Downloads/NonSteamLaunchersInstallation
+rm -rf ~/Downloads/NonSteamLaunchersInstallation
 
 
 
@@ -1837,8 +1837,8 @@ with open('$shortcuts_vdf_path', 'wb') as f:
 
 
 # Detach script from Steam process
-#nohup sh -c 'sleep 10; /usr/bin/steam' &
+nohup sh -c 'sleep 10; /usr/bin/steam' &
 
 # Close all instances of Steam
-#killall steam
+killall steam
 
