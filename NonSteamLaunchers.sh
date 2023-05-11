9df2aae53f4fe3c8aff4e4e52b3f1a7ee5e303cc
moraroy <88516395+moraroy@users.noreply.github.com>
2023-05-10 22:48:52 -0700

    Update NonSteamLaunchers.sh

--- NonSteamLaunchers.sh
+++ NonSteamLaunchers.sh
@@ -764,7 +764,7 @@ proton_dir=$(find ~/.steam/root/compatibilitytools.d -maxdepth 1 -type d -name "
 
 # Set the URLs to download GE-Proton from
 ge_proton_url1=https://github.com/GloriousEggroll/proton-ge-custom/releases/latest/download/GE-Proton.tar.gz
-ge_proton_url2=https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton8-2/GE-Proton8-2.tar.gz
+ge_proton_url2=https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton8-3/GE-Proton8-3.tar.gz
 
 
 
