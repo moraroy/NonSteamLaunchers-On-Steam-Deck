ef2a52193e556cb5bf2f06c5c25c5d7181a001b8
Roy <88516395+moraroy@users.noreply.github.com>
2023-11-08 18:50:00 -0800

    Updated to GE-Proton8-23

--- NonSteamLaunchers.sh
+++ NonSteamLaunchers.sh
@@ -1345,7 +1345,7 @@ proton_dir=$(find "${logged_in_home}/.steam/root/compatibilitytools.d" -maxdepth
 
 # Set the URLs to download GE-Proton from
 ge_proton_url1="https://github.com/GloriousEggroll/proton-ge-custom/releases/latest/download/GE-Proton.tar.gz"
-ge_proton_url2="https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton8-22/GE-Proton8-22.tar.gz"
+ge_proton_url2="https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton8-23/GE-Proton8-23.tar.gz"
 
 # Check if GE-Proton is installed
 if [ -z "$proton_dir" ]; then
@@ -2760,7 +2760,7 @@ if [ -f "$config_vdf_path" ]; then
     cp "$config_vdf_path" "$backup_path"
 
     # Set the name of the compatibility tool to use
-    compat_tool_name="GE-Proton8-22"
+    compat_tool_name="GE-Proton8-23"
 else
     echo "Could not find config.vdf file"
 fi
