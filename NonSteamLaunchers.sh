b72326464d13018dae05a55633f8f23fbf181bbf
Roy <88516395+moraroy@users.noreply.github.com>
2024-01-26 01:20:39 -0800

    Update to 3.5

--- NonSteamLaunchers.sh
+++ NonSteamLaunchers.sh
@@ -22,7 +22,7 @@ download_dir="${logged_in_home}/Downloads/NonSteamLaunchersInstallation"
 exec >> "${logged_in_home}/Downloads/NonSteamLaunchers-install.log" 2>&1
 
 # Version number (major.minor)
-version=v3.4
+version=v3.5
 
 # TODO: tighten logic to check whether major/minor version is up-to-date via `-eq`, `-lt`, or `-gt` operators
 # Check repo releases via GitHub API then display current stable version
