6204392c3dfdceca21a17d1285b93b6a0cc759ed
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-02-21 00:44:29 -0600

    refactor: split config into file
    - Lots of LOC that make the main script hard to read
    - Remove `main.py` as that can come later

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from decouple import config
 from pathlib import Path
 
 # paths
@@ -48,19 +49,87 @@ chromedirectory = "/usr/bin/flatpak"
 
 # Define a dictionary of original folder names
 folder_names = {
+    'Amazon Games': 'AmazonGamesLauncher',
+    'Battle.net': 'Battle.netLauncher',
+    'DMM Games': 'DMMGameLauncher',
+    'EA App': 'TheEAappLauncher',
     'Epic Games': 'EpicGamesLauncher',
     'Gog Galaxy': 'GogGalaxyLauncher',
-    'Ubisoft Connect': 'UplayLauncher',
-    'Origin': 'OriginLauncher',
-    'Battle.net': 'Battle.netLauncher',
-    'EA App': 'TheEAappLauncher',
-    'Amazon Games': 'AmazonGamesLauncher',
-    'itch.io': 'itchioLauncher',
-    'Legacy Games': 'LegacyGamesLauncher',
     'Humble Bundle': 'HumbleGamesLauncher',
     'IndieGala Client': 'IndieGalaLauncher',
-    'Rockstar Games Launcher': 'RockstarGamesLauncher',
+    'itch.io': 'itchioLauncher',
+    'Legacy Games': 'LegacyGamesLauncher',
     'Minecraft: Java Edition': 'MinecraftLauncher',
+    'Origin': 'OriginLauncher',
     'Playstation Plus': 'PlaystationPlusLauncher',
-    'DMM Games': 'DMMGameLauncher',
+    'Rockstar Games Launcher': 'RockstarGamesLauncher',
+    'Ubisoft Connect': 'UplayLauncher',
+    'VK Play': 'VKPlayLauncher',
+}
+
+# Variables from NonSteamLaunchers.sh
+steamid3 = config('steamid3', default='')
+logged_in_home = config('logged_in_home')
+compat_tool_name = config('compat_tool_name')
+controller_config_path = config('controller_config_path')
+python_version = config('python_version')
+#Scanner Variables
+epic_games_launcher = config('epic_games_launcher')
+ubisoft_connect_launcher = config('ubisoft_connect_launcher')
+ea_app_launcher = config('ea_app_launcher')
+gog_galaxy_launcher = config('gog_galaxy_launcher')
+bnet_launcher = config('bnet_launcher')
+amazon_launcher = config('amazon_launcher')
+
+# Variables of the Launchers
+# Define the path of the Launchers
+epicshortcutdirectory = config('epicshortcutdirectory')
+gogshortcutdirectory = config('gogshortcutdirectory')
+uplayshortcutdirectory = config('uplayshortcutdirectory')
+battlenetshortcutdirectory = config('battlenetshortcutdirectory')
+eaappshortcutdirectory = config('eaappshortcutdirectory')
+amazonshortcutdirectory = config('amazonshortcutdirectory')
+itchioshortcutdirectory = config('itchioshortcutdirectory')
+legacyshortcutdirectory = config('legacyshortcutdirectory')
+humbleshortcutdirectory = config('humbleshortcutdirectory')
+indieshortcutdirectory = config('indieshortcutdirectory')
+rockstarshortcutdirectory = config('rockstarshortcutdirectory')
+glyphshortcutdirectory = config('glyphshortcutdirectory')
+minecraftshortcutdirectory = config('minecraftshortcutdirectory')
+psplusshortcutdirectory = config('psplusshortcutdirectory')
+vkplayhortcutdirectory = config('vkplayhortcutdirectory')
+
+# Streaming
+chromedirectory = config('chromedirectory')
+websites_str = config('custom_websites_str')
+custom_websites = websites_str.split(', ') if websites_str else []
+
+# Define your mapping
+flavor_mapping = {
+    "Blizzard Arcade Collection": "RTRO",
+    "Call of Duty: Black Ops - Cold War": "ZEUS",
+    "Call of Duty: Black Ops 4": "VIPR",
+    "Call of Duty: Modern Warfare III": "SPOT",
+    "Call of Duty: Modern Warfare": "ODIN",
+    "Call of Duty: MW 2 Campaign Remastered": "LAZR",
+    "Call of Duty: Vanguard": "FORE",
+    "Call of Duty": "AUKS",
+    "Crash Bandicoot 4: It's About Time": "WLBY",
+    "Diablo II: Resurrected": "OSI",
+    "Diablo III": "D3",
+    "Diablo Immortal (PC)": "ANBS",
+    "Diablo IV": "Fen",
+    "Diablo": "D1",
+    "Hearthstone": "WTCG",
+    "Heroes of the Storm": "Hero",
+    "Overwatch 2": "Pro",
+    "Overwatch": "Pro",
+    "StarCraft 2": "S2",
+    "StarCraft": "S1",
+    "Warcraft Arclight Rumble": "GRY",
+    "Warcraft II: Battle.net Edition": "W2",
+    "Warcraft III: Reforged": "W3",
+    "Warcraft: Orcs & Humans": "W1",
+    "World of Warcraft Classic": "WoWC",
+    "World of Warcraft": "WoW",
 }
