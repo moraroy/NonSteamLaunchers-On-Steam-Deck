a023490f6dcac536268f31108fadb8a3f26ecf1c
Roy <88516395+moraroy@users.noreply.github.com>
2024-03-16 15:31:00 -0700

    temp fix on battlenet

--- NonSteamLaunchers.sh
+++ NonSteamLaunchers.sh
@@ -1591,6 +1591,10 @@ if [[ $options == *"Battle.net"* ]]; then
     # Run the BATTLE file using Proton with the /passive option
     echo "Running BATTLE file using Proton with the /passive option"
     "$STEAM_RUNTIME" "$proton_dir/proton" run "$battle_file" Battle.net-Setup.exe --lang=enUS --installpath="C:\Program Files (x86)\Battle.net"
+	wait
+ 	# Run the BATTLE file using Proton with the /passive option
+    echo "Running BATTLE file using Proton with the /passive option"
+    "$STEAM_RUNTIME" "$proton_dir/proton" run "$battle_file" Battle.net-Setup.exe --lang=enUS --installpath="C:\Program Files (x86)\Battle.net"
 
     while true; do
     if pgrep -f "Battle.net.exe" > /dev/null; then
