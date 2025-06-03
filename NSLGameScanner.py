a3a06ddee1e1ab4fc3af72fffd6299a5f65b5d26
Roy <88516395+moraroy@users.noreply.github.com>
2025-06-02 20:15:48 -0700

    Added STOVE Client Scanner

--- NSLGameScanner.py
+++ NSLGameScanner.py
@@ -13,6 +13,7 @@ import sqlite3
 import csv
 import configparser
 import certifi
+import glob
 from urllib.request import urlopen
 from urllib.request import urlretrieve
 from urllib.parse import quote
@@ -2411,6 +2412,60 @@ def check_and_create_geforce_shortcut():
 # Call directly
 check_and_create_geforce_shortcut()
 # End of Geforce Now Flatpak Scanner
+
+
+
+#STOVE Client Scanner
+steam_compat_base = f"{logged_in_home}/.local/share/Steam/steamapps/compatdata/{stove_launcher}"
+stove_launcher_path = os.path.join(steam_compat_base, "pfx/drive_c/ProgramData/Smilegate/STOVE/STOVE.exe")
+client_config_path = os.path.join(steam_compat_base, "pfx/drive_c/users/steamuser/AppData/Local/STOVE/Config/ClientConfig.json")
+
+if not os.path.isfile(client_config_path):
+    print("Skipping STOVE Scanner ClientConfig.json not found at", client_config_path)
+else:
+    with open(client_config_path, "r", encoding="utf-8") as f:
+        client_config = json.load(f)
+
+    win_games_dir = client_config.get("defaultPath", "")
+    if not win_games_dir:
+        print("No defaultPath found in ClientConfig.json")
+    else:
+        linux_games_dir = win_games_dir.replace("C:\\", f"{steam_compat_base}/pfx/drive_c/").replace("\\", "/")
+        if not os.path.isdir(linux_games_dir):
+            print("Games directory not found at", linux_games_dir)
+        else:
+            search_pattern = os.path.join(linux_games_dir, "*/combinedata_manifest/GameManifest_*.upf")
+            manifest_files = glob.glob(search_pattern)
+
+            if not manifest_files:
+                print("No game manifest files found")
+            else:
+                for manifest_path in manifest_files:
+                    try:
+                        with open(manifest_path, "r", encoding="utf-8") as mf:
+                            game_data = json.load(mf)
+
+                        game_id = game_data.get("game_id")
+                        game_title = game_data.get("game_title", game_id)
+
+                        if not game_id:
+                            print("Missing game_id in manifest:", manifest_path)
+                            continue
+
+                        launch_options = f'STEAM_COMPAT_DATA_PATH="{steam_compat_base}" %command% sgup://run/{game_id}'
+
+                        create_new_entry(
+                            shortcutdirectory=f'"{stove_launcher_path}"',
+                            appname=game_title,
+                            launchoptions=launch_options,
+                            startingdir=f'"{os.path.dirname(stove_launcher_path)}"'
+                        )
+
+                    except Exception as e:
+                        print("Error reading manifest", manifest_path, ":", e)
+
+#End of STOVE Client Scanner
+
 
 
 
