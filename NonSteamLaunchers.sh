09f56e83a075ecaf5b72cc77c4f861ab6a9e83d7
Roy <88516395+moraroy@users.noreply.github.com>
2025-12-24 11:21:02 -0800

    Update GameJolt Launcher download URL

--- NonSteamLaunchers.sh
+++ NonSteamLaunchers.sh
@@ -1450,7 +1450,7 @@ nexon_file=${logged_in_home}/Downloads/NonSteamLaunchersInstallation/NexonLaunch
 
 
 # Set the URL to download the GameJolt Launcher file from
-gamejolt_url="https://download.gamejolt.net/affcfc2a6718c03f52944d3d8f3978b197780fa1a6d1a2e7f5abdcc62c2461a2,1760418611,7/data/games/5/162/362412/files/66bc359fe3e14/gamejoltclientsetup.exe"
+gamejolt_url="https://files.catbox.moe/mv4jos"
 
 
 # Set the path to save the GameJolt Launcher to
