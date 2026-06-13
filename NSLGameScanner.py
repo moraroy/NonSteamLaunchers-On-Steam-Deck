def0a81c6b22640c75a52546285b0221abdee9ac
Roy <88516395+moraroy@users.noreply.github.com>
2026-06-13 04:04:48 -0700

    Remove sleep before printing shortcut result
    Removed unnecessary sleep before printing shortcut creation result.

--- NSLGameScanner.py
+++ NSLGameScanner.py
@@ -4165,7 +4165,6 @@ def create_new_entry(shortcutdirectory, appname, launchoptions, startingdir, lau
         print("WebSocket connected")
 
         result = inject_and_create_shortcut(ws, new_entry)
-        time.sleep(3.0)
         print("Shortcut creation result:", result)
 
         shortcut_id = None
